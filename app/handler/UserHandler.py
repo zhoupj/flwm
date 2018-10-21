2fd20b469cbed4a1cb4c35edd02d9443ba324fb5
pengju.zpj <pengju.zpj@alibaba-inc.com>
2018-10-22 01:13:09 +0800

    udpate post method

--- app/handler/UserHandler.py
+++ app/handler/UserHandler.py
@@ -10,8 +10,8 @@ class Register(RequestHandler):
     def post(self, *args, **kwargs):
         try:
             # 暂时用openId，当作code，之后要改造
-            code = self.post_argument("code");
-            name = self.post_argument("name");
+            code = self.get_argument("code");
+            name = self.get_argument("name");
 
             # 调用微信接口换取openId
             # TODO
@@ -26,7 +26,7 @@ class Register(RequestHandler):
 class Login(RequestHandler):
     def post(self, *args, **kwargs):
         try:
-            code = self.post_argument('code');
+            code = self.get_argument('code');
             df = US.query(code);
             US.update_login_days(df.loc[0,'id']);
             self.write(Result.succ(df.to_json(orient='records')))
@@ -37,7 +37,7 @@ class Login(RequestHandler):
 class Quit(RequestHandler):
     def post(self, *args, **kwargs):
         try:
-            id =int(self.post_argument('user_id'));
+            id =int(self.get_argument('user_id'));
             df = US.query_by_id(id)
             US.update_last_login_time(id);
             self.write(Result.succ('udpate sucess'));
@@ -57,8 +57,8 @@ class MemberActivity(RequestHandler):
 class BuyMember(RequestHandler):
     def post(self, *args, **kwargs):
         try:
-            user_id =int(self.post_argument('user_id'));
-            act_id=int(self.post_argument('act_id'));
+            user_id =int(self.get_argument('user_id'));
+            act_id=int(self.get_argument('act_id'));
             df = US.query_by_id(user_id);
             if(df.loc[0,'is_member']==1):
                 self.write(Result.fail2(Result.CREATE_MEMBER_FAIL))
@@ -74,8 +74,8 @@ class BuyMember(RequestHandler):
 
 class Suggest(RequestHandler):
     def post(self, *args, **kwargs):
-        txt=self.post_argument('txt');
-        user_id=self.post_argument('user_id');
+        txt=self.get_argument('txt');
+        user_id=self.get_argument('user_id');
         df = US.query_by_id(user_id);
         name=df.loc[0,'name'];
         suggest_log.info('%d,%s,%s'%(user_id,name,txt));
